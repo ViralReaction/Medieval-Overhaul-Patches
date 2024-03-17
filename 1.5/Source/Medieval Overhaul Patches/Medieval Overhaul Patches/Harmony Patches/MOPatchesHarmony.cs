11a5d9451cd52849614d4fb92705ebe2a527b114
ViralReaction <136116069+ViralReaction@users.noreply.github.com>
2024-03-17 15:20:35 -0600

    Initial 1.5 Update

---
+++ 1.5/Source/Medieval Overhaul Patches/Medieval Overhaul Patches/Harmony Patches/MOPatchesHarmony.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using ProcessorFramework;
+
+namespace Medieval_Overhaul_Patches
+{
+    [StaticConstructorOnStartup]
+    internal static class MOPatchesHarmony
+    {
+        static MOPatchesHarmony()
+        {
+            Harmony harmony = new Harmony("rw.mopatches");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            harmony.Patch((MethodBase)AccessTools.Method(typeof(CompProcessor), "TakeOutProduct"), postfix: new HarmonyMethod(typeof(MOPatchesHarmony), "TakeOutProduct_Postfix"));
+        }
+
+        public static void TakeOutProduct_Postfix(ActiveProcess activeProcess, ref Thing __result)
+        {
+            if ((__result.TryGetComp<CompIngredients>() is CompIngredients compIngredients) && compIngredients.ingredients.NullOrEmpty())
+            {
+                List<Thing> ingredientThingList = activeProcess.ingredientThings;
+                for (int index = 0; index < ingredientThingList.Count; index++)
+                {
+                    Thing ingredientThing = ingredientThingList[index];
+                    compIngredients.RegisterIngredient(ingredientThing.def);
+
+                }
+            }
+        }
+    }
+}
---
+++ 1.5/Source/Medieval Overhaul Patches/Medieval Overhaul Patches/StartupMessage.cs
@@ -0,0 +1,16 @@
+﻿using RimWorld;
+using Verse;
+
+
+namespace Medieval_Overhaul_Patches
+{
+
+    [StaticConstructorOnStartup]
+    public static class MOPatches
+    {
+        static MOPatches()
+        {
+            Log.Message("Loading Medieval Overhaul Patches");
+        }
+    }
+}
